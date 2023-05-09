40bf506fe4440552202ab467ca9e5802c9cf96c9
Imam-Hossain <zunayedemon6t9@gmail.com>
2023-05-09 22:46:26 +0600

    modify the problem1

--- problem_1.ts
+++ problem_1.ts
@@ -1,8 +1,8 @@
 // const printStringRepeatedly = (input1: string, input2:number=3): void => {
-//     let i:number=0;
+//     let i:number=1;
 //     for(i ;i<=input2 ; i++){
 //         console.log(input1);
 //     }
 
 // }
-// printStringRepeatedly('Team PH', 5)
+// printStringRepeatedly('Team PH')
